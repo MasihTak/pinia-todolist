3fbf8206b54a197c0fb542202e607a39b5d532e2
MasihTak <masihtak@outlook.com>
2023-03-26 18:11:19 +0200

    feat: reactive properties

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+import { useTodoListStore } from "@/stores/todoList";
+import { storeToRefs } from "pinia";
 
-<template> </template>
+const store = useTodoListStore();
 
-<style scoped></style>
+// storeToRefs lets todoList keep reactivity:
+const { todoList } = storeToRefs(store);
+
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted } = store;
+</script>
+
+<template>
+  <div v-for="todo in todoList" :key="todo.id" class="list">
+    <div class="item">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item span:first-of-type {
+    margin-right: 5px;
+}
+
+.completed {
+    text-decoration: line-through;
+}
+
+</style>
